f91287059398beccf86f86fe8f3ad14a0c5489cd
Ajay Gupta <ajaygupta230085@gmail.com>
2024-04-21 00:55:36 +0530

    Add files via upload

---
+++ Array/Possible subarrays.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter no. of elements: ";
+    cin >> n;
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout << arr[k] << " ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
