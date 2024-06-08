ffd8e47f26db63d9ebd46e3e69fe2ce28df02108
Ajay Gupta <ajaygupta230085@gmail.com>
2024-06-08 19:57:05 +0530

    Add files via upload

---
+++ Array/Maximum subarray sum.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter no. of elements: " << endl;
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements: " << " ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int max_sum = INT_MIN;
+    // 1--> Time complexity: O(n^3)
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i; j < n; j++)
+    //     {
+    //         int sum = 0;
+    //         for (int k = i; k <= j; k++)
+    //         {
+    //             sum += arr[k];
+    //         }
+    //         max_sum = max(max_sum, sum);
+    //     }
+    // }
+
+    // 2--> Kadene's Algorithm: O(n)
+    int currSum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        currSum += arr[i];
+        if (currSum < 0)
+        {
+            currSum = 0;
+        }
+        max_sum = max(currSum, max_sum);
+    }
+    cout << "Maximum subarray sum is: " << max_sum << endl;
+    return 0;
+}
